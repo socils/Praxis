fee593ba84b9854500f482c4fc8651ddefe99383
Alonso <ajuliuszch@gmail.com>
2026-05-27 17:30:42 -0500

    Correcion

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// En lugar de poner la clave real, ponemos este "marcador"
-// GitHub Actions se encargará de reemplazar esto por la clave real al desplegar
-export const GEMINI_API_KEY = "REEMPLAZAR_CON_SECRET_GITHUB";
+ const GEMINI_API_KEY = "REEMPLAZAR_CON_SECRET_GITHUB";
