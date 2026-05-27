6730464e287884db226580111234e68d6edf8452
Alonso <ajuliuszch@gmail.com>
2026-05-27 17:05:41 -0500

    Activar api key

---
+++ config.js
@@ -0,0 +1,3 @@
+// En lugar de poner la clave real, ponemos este "marcador"
+// GitHub Actions se encargará de reemplazar esto por la clave real al desplegar
+export const GEMINI_API_KEY = "REEMPLAZAR_CON_SECRET_GITHUB";
